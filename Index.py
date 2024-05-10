bea3ba076f3ace46e99b109cd6552d3fb9cd2b6a
VitorCardosoGitHub <vitorcsantos.github@gmail.com>
2024-05-10 01:20:36 -0300

    Setting up the model parameters

--- Index.py
+++ Index.py
@@ -1,4 +1,4 @@
-# Before running: Run the command 'pip install -q -U google-generativeai' in your terminal
+# Before running: Run the command 'pip install -q -U google-generativeai' in your terminal.
 
 import google.generativeai as genai
 
@@ -8,3 +8,21 @@ genai.configure(api_key=GOOGLE_API_KEY)
 for m in genai.list_models():
     if 'generateContent' in m.supported_generation_methods:
         print(m.name)
+
+generation_config = {
+    "candidate_count": 1,
+    "temperature:": 0.5,
+    #"top_p"
+    #"top_k"
+}
+
+safety_settings = {
+    # Block_None
+    # Block_Few
+    # Block_Some
+    # Block_Most
+    "Harassment": "Block_None",
+    "Hate": "Block_None",
+    "Sexual": "Block_None",
+    "Dangerous": "Block_None",
+}
